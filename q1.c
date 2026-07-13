1d92b7aee40fcebc61c9a1e5abb582d7243a160f
Anushka Sharma <anushkasharma65722@gmail.com>
2026-07-13 14:01:01 +0530

    intial

---
+++ q1.c
@@ -0,0 +1,18 @@
+//person eligible for license 
+#include<stdio.h>
+void main()
+{  
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+     if
+     if((ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') && (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'))
+    {
+        printf("character is vowel");
+    }
+     else 
+    {
+        printf("character is a consonant");
+    }
+
+}
